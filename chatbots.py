2a0eb118b23de44a96cee059861093f8d05c8d6b
rmxrmx <mrokas@protonmail.com>
2021-09-29 00:03:37 +0200

    Rewrote into function; reformatted outputs

--- chatbots.py
+++ chatbots.py
@@ -4,32 +4,25 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B", cache_dir=".cache/huggingface/transformers")
 model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", cache_dir=".cache/huggingface/transformers")
 
+
+def conversation_generation(speaker_one, speaker_two, length, context):
+    c = speaker_one + ": " + context + "\n" + speaker_two + ":"
+
+    for i in range(0, length):
+        input_ids = tokenizer.encode(c, return_tensors='pt')
+
+        top_p_output = model.generate(
+        input_ids, 
+        do_sample=True, 
+        max_length=100*(i+1),
+        top_p = 0.92
+        )
+
+        c = tokenizer.decode(top_p_output[0], skip_special_tokens=False)
+        print("Output " + str(i) + ":\n" + 100 * '-')
+        print(c)
+        print("\n" + 100 * '-')
+
+
 # encode context the generation is conditioned on
-"""input_ids = tokenizer.encode('Psychiatrist: Welcome back, Kevin. How have you been?\nKevin:', return_tensors='pt')
-
-top_p_output = model.generate(
-    input_ids, 
-    do_sample=True, 
-    max_length=100,
-    top_p = 0.92,
-    top_k=50
-)
-
-print("Output:\n" + 100 * '-')
-print(tokenizer.decode(top_p_output[0], skip_special_tokens=True))"""
-
-context = "Psychiatrist: Welcome back, Kevin. How have you been?\nKevin:"
-for i in range(0, 5):
-    input_ids = tokenizer.encode(context, return_tensors='pt')
-
-    top_p_output = model.generate(
-    input_ids, 
-    do_sample=True, 
-    max_length=100 * (i + 1),
-    top_p = 0.92
-    )
-
-    context = tokenizer.decode(top_p_output[0], skip_special_tokens=True)
-    print("Output " + str(i) + ":\n" + 100 * '-')
-    print(context)
-    print("\n" + 100 * '-')
+conversation_generation("Psychiatrist", "Kevin", 5, "Welcome back, Kevin. How have you been?")
